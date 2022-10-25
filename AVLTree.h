0b6beebd4a69b1a0cd5cf8789c3c05c4e0664a27
Peter Ke <peterke@Peters-MacBook-Pro.local>
2022-10-25 18:11:57 -0400

    more tree interface

--- AVLTree.h
+++ AVLTree.h
@@ -1,7 +1,21 @@
-
+#include "AVLNode.h"
 
 class AVLTree {
     
-    
+private:
+    AVLNode * root;
 
+    AVLNode *rotateRR(AVLNode *);
+    AVLNode *rotateLL(AVLNode *);
+    AVLNode *rotateLR(AVLNode *);
+    AVLNode *rotateRL(AVLNode *);
+    AVLNode *balance(AVLNode *);
+
+public:
+    AVLTree();
+    ~AVLTree();
+
+    void insert(int);
+    void delete_(int);
+    AVLNode * find(int);
 };
