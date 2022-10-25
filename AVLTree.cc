27985dd40879410a5bcb3d4246f84cff3df5c3f1
Peter Ke <peter.ke@yahoo.com>
2022-10-25 18:18:51 -0400

    tree constructor & destructor

--- AVLTree.cc
+++ AVLTree.cc
@@ -1,3 +1,15 @@
 #include "AVLTree.h"
 
+AVLTree::AVLTree() {
+    root = nullptr;
+}
 
+AVLTree::~AVLTree() {
+    recursiveDeallocate(root);
+}
+
+void AVLTree::recursiveDeallocate(AVLNode * node) {
+    recursiveDeallocate(node->getLeft());
+    recursiveDeallocate(node->getRight());
+    delete node;
+}
