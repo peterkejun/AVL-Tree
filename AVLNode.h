3786f829fca50a2160c3f2dbf0c8d56122c942d1
Ray Yang <rayyang@Rays-MacBook-Pro.local>
2022-10-25 18:05:45 -0400

    Implemented basic AVLNode features

--- AVLNode.h
+++ AVLNode.h
@@ -2,8 +2,26 @@
 class AVLNode
 {
 private:
-    /* data */
+    int data;
+    AVLNode * left;
+    AVLNode * right;
 public:
-    AVLNode(/* args */);
+    AVLNode(int data, AVLNode*left, AVLNode*right);
     ~AVLNode();
+    int getData();
+    void setData(int newData);
 };
+
+AVLNode::AVLNode(int data, AVLNode*left, AVLNode*right){
+    this->data = data;
+    this->left = left;
+    this->right = right;
+}
+
+int AVLNode::getData(){
+    return this->data;
+}
+
+void AVLNode::setData(int data){
+    this->data = data;
+}
