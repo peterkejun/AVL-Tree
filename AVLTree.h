435f3542eb5d10b2f852d2dd9bd7399a53d0114e
Peter Ke <peter.ke@yahoo.com>
2022-10-25 23:29:07 -0400

    tree iterator

--- AVLTree.h
+++ AVLTree.h
@@ -2,6 +2,9 @@
 #include <iostream>
 using namespace std;
 
+#ifndef __AVL_TREE_H__
+#define __AVL_TREE_H__
+
 template<class T>
 class AVLTree {
     
@@ -267,3 +270,5 @@ AVLTree<T> *AVLTree<T>::clone() const {
     }
     return new AVLTree<T>(recursiveClone(root));
 }
+
+#endif
